6fb157209fdc2a1438e3e5a5082799b1b8b28475
Mech-Hammad <115184729+Mech-Hammad@users.noreply.github.com>
2022-10-06 23:57:36 +0500

    Add files via upload

---
+++ C# 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ 
+namespace P10
+{
+    class Program {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The relative position of positive numbers remains unchanged.");
+            Console.WriteLine("The relative position of negative numbers remains unchanged.");
+            Console.WriteLine("The positive numbers are concentrated to the left while negative numbers are concentrated to the right");
+
+            
+            int[] array = {12,-67, -23, 54,21,-23,8,-23,-9,-2,-3,23,2};
+            Console.WriteLine("Original Array: {0}",String.Join(", ",array));
+
+            for(int i=0;i<array.Length; i++)
+            {
+                if (array[i]>=0) //Negative Number
+                {
+                    int j = i-1;
+                    while(j>=0 && array[j]<0)// Next number is also negative
+                    {
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                        j--;
+                    }
+                }
+            }
+            Console.WriteLine("Arranged Array: {0}",String.Join(", ",array));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem 12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+ 
+namespace P10
+{
+    class Program {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program swaps 1st and last element of an array");
+            Console.WriteLine("We simply store the 1st element in a temporary variable.");
+            Console.WriteLine("The replace the 1st element with last element.");
+            Console.WriteLine("Then replace the last element with 1st temporary stored value");
+            
+
+            
+            int[] array = {12,-67, -23, 54,21,-23,8,-23,-9,-2,-3,23,2};
+            Console.WriteLine("Original Array: {0}",String.Join(", ",array));
+
+            int temp = array[0];
+            array[0] = array[array.Length-1];
+            array[array.Length-1] = temp;
+            
+            Console.WriteLine("Sorted as Required Array: {0}",String.Join(", ",array));
+
+
+           
+            Console.ReadLine();
+        }
+    }
+}
